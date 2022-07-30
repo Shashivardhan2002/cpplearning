725e25aee102ecc9860ae7d9dce6d5823cd5659d
Shashi Vardhan <103044798+Shashivardhan2002@users.noreply.github.com>
2022-07-30 17:27:35 +0530

    Create count numbers.cpp

---
+++ Recursion/count numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void count(int n){
+  if(n==0){
+     return ;
+  }
+ 
+  count(n-1);
+  cout<<n<<endl ;
+    
+}
+int main(){
+    int a;
+    cout<<"enter a number:";
+    cin>>a;
+    count(a);
+}
